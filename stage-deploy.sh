26e9bdc8af85a6c31eb0e6119540396895d693cc
Elnur Ibrahim-zada <e.ibrahimzade@gmail.com>
2023-04-06 13:19:59 +0400

    Eight Commit

--- stage-deploy.sh
+++ stage-deploy.sh
@@ -1,7 +1,6 @@
 #use this code to rebuild docker containers as well.
 
-git reset --hard && git pull \
-&& docker-compose up -d --build \
+docker-compose up -d --build \
 && docker-compose run --rm composer install \
 && docker-compose run --rm npm install \
 && docker-compose run --rm npm run build \
